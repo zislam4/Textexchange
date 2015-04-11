a909de29a0bfbde037119ee133d136dd7b878a2f
jadeyychan <jadeyychan@gmail.com>
2015-04-11 03:58:42 -0400

    pb-pages

--- sellerhome.js
+++ sellerhome.js
@@ -8,6 +8,25 @@ var main = function() {
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
 	})
+
+	$('.btn').click(function() {
+		var post = $('.status-box').val();
+		$('<li>').text(post).prependTo('.posts');
+		$('.status-box').val('');
+		$('.btn').addClass('disabled');
+	});
+
+	$('.status-box').keyup(function() {
+		var post_length = $(this).val().length;
+
+		if (post_length <= 0) {
+			$('.btn').addClass('disabled');
+		} else {
+			$('.btn').removeClass('disabled');
+		}
+	});
+
+	$('.btn').addClass('disabled');
 }
 
 $(document).ready(main);
