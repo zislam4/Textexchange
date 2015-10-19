a4f7127aa2112fc75abd7c218314f11453824b6d
Elif Kinli <elif.kinli@tufts.edu>
2015-10-18 21:31:52 -0400

    sliding down div

--- sellerhome.js
+++ sellerhome.js
@@ -24,6 +24,11 @@ var main = function() {
 
 	request.send();
 
+	$(document).ready(function () {
+		$("add-books-button").click(function() {
+			$("add-books").slideToggle("slow");
+		});
+	});
 
 
 // 	// Begin interactive functions
