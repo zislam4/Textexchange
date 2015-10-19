79f6bd1d73f9b217c615530bfce4dfc05f7439d5
Elif Kinli <elif.kinli@tufts.edu>
2015-10-18 20:24:54 -0400

    working on the sellerpage, added a button

--- sellerhome.js
+++ sellerhome.js
@@ -16,7 +16,7 @@ var main = function() {
 			var bookinfos = JSON.parse(request.responseText);
 			for (var i = 0; i < bookinfos.length; i++) {
 				if (bookinfos[i]["title"] != null) {
-					$('<li>').text(bookinfos[i]["title"]).prependTo('.manage-books');
+					$('<li>').text(bookinfos[i]["title"]).prependTo('.mybookinfo');
 				}
 			}
 		}
@@ -26,75 +26,75 @@ var main = function() {
 
 
 
-	// Begin interactive functions
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+// 	// Begin interactive functions
+// 	$('ul.tabs li').click(function(){
+// 		var tab_id = $(this).attr('data-tab');
 
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
+// 		$('ul.tabs li').removeClass('current');
+// 		$('.tab-content').removeClass('current');
 
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
+// 		$(this).addClass('current');
+// 		$("#"+tab_id).addClass('current');
+// 	})
 
-	$('.title-box').keyup(function() {
-		var title_length = $(this).val().length;
+// 	$('.title-box').keyup(function() {
+// 		var title_length = $(this).val().length;
 
-		if (title_length <= 0) {
-			$('.btn').addClass('disabled');
-		} else {
-			$('.btn').removeClass('disabled');
-		}
-	});
+// 		if (title_length <= 0) {
+// 			$('.btn').addClass('disabled');
+// 		} else {
+// 			$('.btn').removeClass('disabled');
+// 		}
+// 	});
 
-	$('.btn').click(function() {
-		var xhr = new XMLHttpRequest();
-		xhr.open("post", "https://morning-peak-4677.herokuapp.com/add", true);
+// 	$('.btn').click(function() {
+// 		var xhr = new XMLHttpRequest();
+// 		xhr.open("post", "https://morning-peak-4677.herokuapp.com/add", true);
 
-		var title = $('.title-box').val(); 
-		var author = $('.author-box').val();
-		var vol = $('.vol-box').val(); 
-		var edi = $('.edi-box').val(); 
-		var clname = $('.class-box').val(); 
-		var cnum = $('.course-box').val(); 
-		var prof = $('.prof-box').val(); 
-		var price = $('.price-box').val(); 
-		var cond = $('.cond-box').val(); 		
+// 		var title = $('.title-box').val(); 
+// 		var author = $('.author-box').val();
+// 		var vol = $('.vol-box').val(); 
+// 		var edi = $('.edi-box').val(); 
+// 		var clname = $('.class-box').val(); 
+// 		var cnum = $('.course-box').val(); 
+// 		var prof = $('.prof-box').val(); 
+// 		var price = $('.price-box').val(); 
+// 		var cond = $('.cond-box').val(); 		
 
-		var bookinfo = "seller_name="+seller_name+"&seller_id="+seller_id+"&title="+title+"&author="+author+"&vol="+vol		+"&edition="+edi+"&classname="+clname+"&coursenum="+cnum+"&prof="+prof+"&price="+price+"&condition="+cond;
+// 		var bookinfo = "seller_name="+seller_name+"&seller_id="+seller_id+"&title="+title+"&author="+author+"&vol="+vol		+"&edition="+edi+"&classname="+clname+"&coursenum="+cnum+"&prof="+prof+"&price="+price+"&condition="+cond;
 
 		
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.setRequestHeader("Content-length", bookinfo.length);
-		xhr.setRequestHeader("Connection", "close");
+// 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+// 		xhr.setRequestHeader("Content-length", bookinfo.length);
+// 		xhr.setRequestHeader("Connection", "close");
 
-		xhr.send(bookinfo);
-/*
-		$('<li>').text(title).prependTo('.posts');
-		$('<li>').text(title).prependTo('.manage-books');
-		$('<li>').text(author).prependTo('.posts');
-		$('<li>').text(vol).prependTo('.posts');
-		$('<li>').text(edi).prependTo('.posts');
-		$('<li>').text(clname).prependTo('.posts');
-		$('<li>').text(cnum).prependTo('.posts');
-		$('<li>').text(prof).prependTo('.posts');
-		$('<li>').text(price).prependTo('.posts');
-		$('<li>').text(cond).prependTo('.posts');
-*/
-		$('.title-box').val('');
-		$('.author-box').val('');
-		$('.vol-box').val('');
-		$('.edi-box').val('');
-		$('.class-box').val('');
-		$('.course-box').val('');
-		$('.prof-box').val('');
-		$('.price-box').val('');
-		$('.cond-box').val('');
+// 		xhr.send(bookinfo);
+// /*
+// 		$('<li>').text(title).prependTo('.posts');
+// 		$('<li>').text(title).prependTo('.manage-books');
+// 		$('<li>').text(author).prependTo('.posts');
+// 		$('<li>').text(vol).prependTo('.posts');
+// 		$('<li>').text(edi).prependTo('.posts');
+// 		$('<li>').text(clname).prependTo('.posts');
+// 		$('<li>').text(cnum).prependTo('.posts');
+// 		$('<li>').text(prof).prependTo('.posts');
+// 		$('<li>').text(price).prependTo('.posts');
+// 		$('<li>').text(cond).prependTo('.posts');
+// */
+// 		$('.title-box').val('');
+// 		$('.author-box').val('');
+// 		$('.vol-box').val('');
+// 		$('.edi-box').val('');
+// 		$('.class-box').val('');
+// 		$('.course-box').val('');
+// 		$('.prof-box').val('');
+// 		$('.price-box').val('');
+// 		$('.cond-box').val('');
 
-		$('.btn').addClass('disabled');	
-	});
+// 		$('.btn').addClass('disabled');	
+// 	});
 
-	$('.btn').addClass('disabled');
+// 	$('.btn').addClass('disabled');
 }
 
-//$(document).ready(main);
+$(document).ready(main);
