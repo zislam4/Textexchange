e353e14f3a55d2c157a9aae55d68815df30484a6
Jade <jadeyychan@gmail.com>
2015-10-31 19:12:32 -0400

    using document.createElement

--- sellerhome.js
+++ sellerhome.js
@@ -40,11 +40,19 @@ var main = function() {
 	// Begin interactive functions
 
 	$('.add-books-button').click(function(){
-		var addbookform = "this is the form";
-		$(this).next().appendChild(addbookform);
+		var node = document.createElement("addbookform");
+		$(this).appendChild(node);
 		console.log("after appending?");
 		$(this).next().toggle();
 	});
+
+
+
+// var node = document.createElement("LI");                 // Create a <li> node
+// var textnode = document.createTextNode("Water");         // Create a text node
+// node.appendChild(textnode);                              // Append the text to <li>
+// document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
+
 
 // var p = document.createElement("p");
 // document.body.appendChild(p);
