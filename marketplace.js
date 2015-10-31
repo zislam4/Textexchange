ad900a7b9d63bec3bdcc4ed1ec99dc64262660dd
Elif Kinli <elif.kinli@tufts.edu>
2015-10-31 17:33:18 -0400

    adding info in hidedive div

--- marketplace.js
+++ marketplace.js
@@ -3,25 +3,27 @@ var main = function() {
 	request.open("GET", "https://morning-peak-4677.herokuapp.com/", true);
 
 	request.onreadystatechange = function () {
-		console.log(request.readyState);
-		console.log(request.status);
+		// console.log(request.readyState);
+		// console.log(request.status);
 		if (request.readyState == 4 && request.status == 200) {
 			var bookinfos = JSON.parse(request.responseText);
-			console.log(bookinfos[1]["title"]);
+			// console.log(bookinfos[1]["title"]);
 			for (var i = 0; i < bookinfos.length; i++) {
 				if (bookinfos[i]["seller_name"] != null) {
 					var li = $('<li>').text(bookinfos[i]["title"]);
 					li.addClass("bookItem");
-
+					console.log(bookinfos[i]);
 					$(".bookinfo").append(li);
-					$(".bookinfo").append("<div class='hide'> hello </div>");
-					console.log(bookinfos[i]["title"]);
+					$(".bookinfo").append(
+						"<div class='hide'>" + 
+						"<p class='seller_name'>" + bookinfos[i]["seller_name"] + "</p>"
+						+"</div>");
 
 				}
 			}
 			$(".bookinfo").find(".hide").hide();
 			$("li.bookItem").click(function() {
-				console.log($(this).text());
+				console.log($(this));
 				$(this).next().append("li.bookItem".title);
 				$(this).next().toggle();
 			});	
