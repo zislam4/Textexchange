2d95e3190b0151adcbc9f189d787d6ebe7731c8d
Elif Kinli <elif.kinli@tufts.edu>
2015-12-20 20:53:38 -0500

    fb moved to sellerhome.js

--- js/sellerhome.js
+++ js/sellerhome.js
@@ -1,45 +1,129 @@
 var main = function() {
 	$('#add-books-form').hide();
 
-	var seller_name = user_info.name;
-	var seller_id = user_info.id;
-
-	var request = new XMLHttpRequest();
-	var url = "https://morning-peak-4677.herokuapp.com/managebooks?"+"seller_id="+seller_id;
-	request.open("GET", url, true);
-
-	request.onreadystatechange = function () {
-		if (request.readyState == 4 && request.status == 200) {
-			var bookinfos = JSON.parse(request.responseText);
-			for (var i = 0; i < bookinfos.length; i++) {
-				if (bookinfos[i]["title"] != null) {
-					// $('<li>').text(bookinfos[i]["title"]).prependTo('.mybookinfo');
-					var li = $('<li>').text(bookinfos[i]["title"]);
-					li.addClass("bookItem");
-
-					$(".mybookinfo").append(li);
-					$(".mybookinfo").append("<div class='hide'> hello </div>");
-				}
-			}
-		
-		$(".bookinfo").find(".hide").hide();
-		$("li.bookItem").click(function() {
-			$(this).next().append("li.bookItem".title);
-			$(this).next().toggle();
-		});	
+	var user_info;
+// This is called with the results from from FB.getLoginStatus().
+	function statusChangeCallback(response) {
+		console.log('statusChangeCallback');
+		console.log(response);
+		// The response object is returned with a status field that lets the
+		// app know the current login status of the person.
+		// Full docs on the response object can be found in the documentation
+		// for FB.getLoginStatus().
+		if (response.status === 'connected') {
+		// Logged into your app and Facebook.
+		testAPI();
+		} else if (response.status === 'not_authorized') {
+		// The person is logged into Facebook, but not your app.
+		document.getElementById('status').innerHTML = 'Please log ' +
+		'into this app.';
+		} else {
+		// The person is not logged into Facebook, so we're not sure if
+		// they are logged into this app or not.
+		document.getElementById('status').innerHTML = 'Please log ' +
+		'into Facebook.';
 		}
+	}
+
+// This function is called when someone finishes with the Login
+// Button. See the onlogin handler attached to it in the sample
+// code below.
+	function checkLoginState() {
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+	}
+
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId : '870286709733829',
+			cookie : true, // enable cookies to allow the server to access
+			// the session
+			xfbml : true, // parse social plugins on this page
+			version : 'v2.2' // use version 2.2
+		});
+
+		// Now that we've initialized the JavaScript SDK, we call
+		// FB.getLoginStatus(). This function gets the state of the
+		// person visiting this page and can return one of three states to
+		// the callback you provide. They can be:
+		//
+		// 1. Logged into your app ('connected')
+		// 2. Logged into Facebook, but not your app ('not_authorized')
+		// 3. Not logged into Facebook and can't tell if they are logged into
+		// your app or not.
+		//
+		// These three cases are handled in the callback function.
+
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+
 	};
 
-	request.send();
+	// Load the SDK asynchronously
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	// Here we run a very simple test of the Graph API after login is
+	// successful. See statusChangeCallback() for when this call is made.
+	function testAPI() {
+		console.log('Welcome! Fetching your information.... ');
+		FB.api('/me', function(response) {
+			console.log('Successful login for: ' + response.name);
+			user_info = response;
+			main();
+			document.getElementById('status').innerHTML =
+			'Thanks for logging in, ' + response.name + '!';
+			get_seller_books();
+		});
+	}
+
+
+	function get_seller_books () {
+		var seller_name = user_info.name;
+		var seller_id = user_info.id;
+
+		var request = new XMLHttpRequest();
+		var url = "https://morning-peak-4677.herokuapp.com/managebooks?seller_id="+seller_id;
+		request.open("GET", url, true);
+
+		request.onreadystatechange = function () {
+			if (request.readyState == 4 && request.status == 200) {
+				var bookinfos = JSON.parse(request.responseText);
+				for (var i = 0; i < bookinfos.length; i++) {
+					if (bookinfos[i]["title"] != null) {
+						// $('<li>').text(bookinfos[i]["title"]).prependTo('.mybookinfo');
+						var li = $('<li>').text(bookinfos[i]["title"]);
+						li.addClass("bookItem");
+
+						$(".mybookinfo").append(li);
+						$(".mybookinfo").append("<div class='hide'> hello </div>");
+					}
+				}
+			
+			$(".bookinfo").find(".hide").hide();
+			$("li.bookItem").click(function() {
+				$(this).next().append("li.bookItem".title);
+				$(this).next().toggle();
+			});	
+			}
+		};
+
+		request.send();
+	}
+
 
 
 	// Begin interactive functions
 
 	$('.add-books-button').click(function(){
 			$(this).next().toggle();
-			console.log("in changed");
-			console.log($(this));
-			console.log($(this).next());
 	});
 
 
